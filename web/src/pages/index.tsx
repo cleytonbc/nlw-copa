74fd11d8076c878686225e7ae1ebe699daa98680
Cleyton b Coelho <cleytonbc@hotmail.com>
2022-11-06 18:27:08 -0300

    Configurado os chamado a api e a criação da api

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,41 @@
-// interface HomeProps {
-//   count: number
-// }
+interface HomeProps {
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
+}
 
 import Image from 'next/image'
 import appPreviewImg from '../assets/app-nlw-copa-preview.png'
 import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
+import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react';
 
-export default function Home() {
+export default function Home({poolCount, guessCount, userCount}: HomeProps) {
+
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      })
+
+      const { code } = response.data
+
+      navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso, o código foi copiado para a área de transferência')
+
+      setPoolTitle('')
+    }
+    catch(err) {
+      console.log(err)
+      alert('Falha ao criar o bolão, tente novamente')
+    }
+  }
 
   return (
     <div  className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
@@ -22,16 +49,18 @@ export default function Home() {
         <div className='mt-10 flex items-center gap-2'>
           <Image src={usersAvatarExampleImg} alt="" />
           <strong className='text-gray-100 text-xl'>
-            <span className='text-ignite-500'>+12.592</span> pessoas já estão usando
+            <span className='text-ignite-500'>+{userCount}</span> pessoas já estão usando
           </strong>
         </div>
 
-        <form className='mt-10 flex gap-2'>
+        <form onSubmit={createPool} className='mt-10 flex gap-2'>
           <input
-            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
             type="text"
             required
             placeholder='Qual nome do seu bolão?'
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button
             className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
@@ -49,7 +78,7 @@ export default function Home() {
           <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt="" />
             <div className='flex flex-col '>
-              <span className='font-bold text-2xl'>+2.034</span>
+              <span className='font-bold text-2xl'>+{poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
@@ -57,7 +86,7 @@ export default function Home() {
           <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt="" />
             <div className='flex flex-col ' >
-              <span className='font-bold text-2xl'>+192.847</span>
+              <span className='font-bold text-2xl'>+{guessCount}</span>
               <span>Palpites enviados</span>
             </div>
           </div>
@@ -76,15 +105,18 @@ export default function Home() {
 }
 
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch('http://localhost:3333/pools/count');
-//   const data = await response.json();
+export const getServerSideProps = async () => {
+  const [pollCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count'),
+  ])
 
-//   console.log(data);
-
-//   return {
-//     props: {
-//       count: data.count
-//     }
-//   }
-// }
+  return {
+    props: {
+      poolCount: pollCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count
+    }
+  }
+}
