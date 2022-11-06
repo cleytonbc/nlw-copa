076e226ed9dad4ade91a9575a757a0a498589b04
Cleyton b Coelho <cleytonbc@hotmail.com>
2022-11-06 16:17:37 -0300

    criado a estrutura sem estilo

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,16 +4,58 @@
 
 import Image from 'next/image'
 import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
 
 export default function Home() {
 
   return (
     <div>
       <main>
+        <Image src={logoImg} alt="NLW Copa" />
+        
+        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" required placeholder='Qual nome do seu bolão?'/>
+          <button type="submit">Criar meu bolão</button>
+        </form>
+
+        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
 
       </main>
 
-      <Image src={appPreviewImg} alt="Dois celulares exibindo ma prévia da aplicação móvel do NLWcoap" />
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo ma prévia da aplicação móvel do NLWcoap"
+        quality={100}
+      />
+
     </div>
   )
 }
