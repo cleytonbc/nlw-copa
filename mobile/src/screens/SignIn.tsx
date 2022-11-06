f02ea20fa1997ef0ae6b476bdd6e21dd222c5499
Cleyton b Coelho <cleytonbc@hotmail.com>
2022-11-06 18:53:32 -0300

    iniciando projeto mobile

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Center, Text} from "native-base";
+
+export function SignIn(){
+    return (
+        <Center flex={1} bgColor="gray.900" fontFamily="heading">
+            <Text color="white" fontSize={24}>
+                SignIn
+            </Text>
+        </Center>
+    )
+}
